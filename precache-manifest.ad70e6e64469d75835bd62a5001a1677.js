39b007d5e36f8426a5cc67d2cac7859d4834403f
Idomason <idomakevin@gmail.com>
2021-12-18 01:07:23 +0000

    Updates

---
+++ precache-manifest.ad70e6e64469d75835bd62a5001a1677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39c91f8880a2b9ae72ea",
+    "url": "/monsters-rolodex/static/css/main.c0df58e8.chunk.css"
+  },
+  {
+    "revision": "39c91f8880a2b9ae72ea",
+    "url": "/monsters-rolodex/static/js/main.39c91f88.chunk.js"
+  },
+  {
+    "revision": "8ee15efa1a2054ecf258",
+    "url": "/monsters-rolodex/static/js/1.8ee15efa.chunk.js"
+  },
+  {
+    "revision": "4933064c23f62bd41ef2",
+    "url": "/monsters-rolodex/static/js/2.4933064c.chunk.js"
+  },
+  {
+    "revision": "84e3ea8691145683fd26",
+    "url": "/monsters-rolodex/static/js/runtime~main.84e3ea86.js"
+  },
+  {
+    "revision": "db7ca4f3b08b183c851b3be3eb879f4f",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
